781f273b68e251e0440f5960ea2c47fbbd7cc6b6
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-12 15:11:06 +0100

    ejercicio 3-16

--- index.js
+++ index.js
@@ -99,6 +99,23 @@ app.get("/info", (request, response) => {
   );
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "Unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted ID" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  response.status(500).send({ error: "Internal server error" });
+};
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
