a902c089c25b657522289b6b3587cd15dfe4962c
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-15 14:42:13 +0100

    ejercicio 4-15

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogController')
+const usersRouter = require('./controllers/user')
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -18,7 +19,9 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- utils/middleware.js
+++ utils/middleware.js
@@ -15,9 +15,10 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
   if (error.name === 'ValidationError') {
     response.status(400).json({ error: error.message })
-  } else {
-    next(error)
+  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
   }
+  next(error)
 }
 
 module.exports = { requestLogger, unknownEndpoint, errorHandler }
