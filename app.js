9589ee78be5bce76b0fd730f3c54d5083e42a787
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-15 23:18:12 +0100

    ejercicio 4-20

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const blogsRouter = require('./controllers/blogController')
 const usersRouter = require('./controllers/userController')
 const loginRouter = require('./controllers/loginController')
 
+
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => logger.info('Connected to MongoDB'))
@@ -19,6 +20,7 @@ mongoose
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,10 +1,22 @@
+require('jsonwebtoken')
 const logger = require('./logger')
+
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:', request.path)
   logger.info('Body:', request.body)
   logger.info('---')
+  next()
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
   next()
 }
 
@@ -27,4 +39,4 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { requestLogger, unknownEndpoint, errorHandler }
+module.exports = { requestLogger, unknownEndpoint, errorHandler, tokenExtractor }
